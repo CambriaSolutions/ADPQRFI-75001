dca86f6993f47f874d90840ecfdcc9d796569895
Vishal Talwar <rawlatv@gmail.com>
2016-05-26 23:54:33 +0000

    Updating home page style

--- osi_prototype/assets.py
+++ osi_prototype/assets.py
@@ -9,6 +9,12 @@ css = Bundle(
     output='public/css/common.css'
 )
 
+css_home = Bundle(
+    'css/home.css',
+    filters='cssmin',
+    output='public/css/home.css'
+)
+
 js = Bundle(
     'libs/jquery/dist/jquery.js',
     'libs/bootstrap/dist/js/bootstrap.js',
@@ -21,3 +27,4 @@ assets = Environment()
 
 assets.register('js_all', js)
 assets.register('css_all', css)
+assets.register('css_home', css_home)
