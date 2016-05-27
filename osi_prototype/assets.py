5ebb9a36c65bf8b463a35b53c55b74b426e9da95
Vishal Talwar <rawlatv@gmail.com>
2016-05-27 19:04:11 +0000

    Switching bootstrap to bootstrap-sass for easier overriding

--- osi_prototype/assets.py
+++ osi_prototype/assets.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 """Application assets."""
+import os
+
 from flask_assets import Bundle, Environment
+from webassets.filter import get_filter
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+CSS_DIR = os.path.join(BASE_DIR, 'static/css')
+BOOTSTRAP_DIR = os.path.join(
+    BASE_DIR, 'static/libs/bootstrap-sass/assets/stylesheets')
+
+
+sass = get_filter('scss', as_output=True,
+                  load_paths=(CSS_DIR, BOOTSTRAP_DIR))
 
 css = Bundle(
-    'libs/bootstrap/dist/css/bootstrap.css',
     'css/style.scss',
-    filters='scss',
+    depends=('css/*.scss'),
+    filters=(sass,),
     output='public/css/common.css'
 )
 
 js = Bundle(
     'libs/jquery/dist/jquery.js',
-    'libs/bootstrap/dist/js/bootstrap.js',
+    'libs/bootstrap-sass/assets/javascripts/bootstrap.js',
     'js/plugins.js',
     filters='jsmin',
     output='public/js/common.js'
