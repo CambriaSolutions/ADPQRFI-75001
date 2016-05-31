3200c641ee6c97f8cfa5b5f5f83caea3b1cee7c5
Vishal Talwar <rawlatv@gmail.com>
2016-05-31 17:19:09 +0000

    Updating prod db url setting

--- osi_prototype/settings.py
+++ osi_prototype/settings.py
@@ -22,7 +22,7 @@ class ProdConfig(Config):
 
     ENV = 'prod'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_CHARCOAL_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
